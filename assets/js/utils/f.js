4b2492ef14b72bde4ab3ee18d0f0d1711bf37e00
wkylin <wkylin@sina.cn>
2014-07-16 15:49:19 +0800

    Rejected commit: Default

---
+++ assets/js/utils/f.js
@@ -0,0 +1,6 @@
+define(["jquery", "app/jquery.alpha", "beta"], function($) {
+    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+    $(function() {
+        $('body').alpha().beta();
+    });
+});
---
+++ assets/js/utils/math.js
@@ -0,0 +1,16 @@
+/**
+ * Created by wangjingang on 2014/7/11.
+ */
+
+define(function () {
+
+    var add = function (x, y) {
+
+        return x + y;
+    };
+    return {
+
+        add: add
+
+    };
+});
