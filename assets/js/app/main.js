5f1c49480d4c5ba9686e0ba4275d2e647cf8a91c
wkylin <wkylin@sina.cn>
2014-07-16 16:30:19 +0800

    GruntJS  && RequireJS

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -2,8 +2,8 @@
 requirejs.config({
     baseUrl: './js',
     paths: {
-        'jquery': 'libs/jquery-1.8.2',
-        'beta':'app/jquery.beta',
+        'jquery': 'libs/jquery-1.8.2.min',
+        'beta':'utils/jquery.beta',//shim
         'a': 'utils/a',
         'b': 'utils/b',
         'c': 'utils/c',
@@ -27,12 +27,14 @@ requirejs.config({
     }
 });
 
+//jQuery
 requirejs(['d', 'e', 'jquery'], function (d, e, $) {
     var i = d.info + ',' + e.info;
     $('.ui-grid-box').html(i);
     console.log($.fn.jquery);
 });
 
+//Math
 requirejs(['math'], function (math) {
     console.log(math.add(1, 2));
 });
--- assets/js/utils/f.js
+++ assets/js/utils/f.js
@@ -1,4 +1,4 @@
-define(["jquery", "app/jquery.alpha", "beta"], function($) {
+define(["jquery", "jquery.alpha.js", "beta"], function($) {
     //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
     $(function() {
         $('body').alpha().beta();
